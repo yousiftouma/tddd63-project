512544944840208311760b253ca68ba88ef508a3
Adam <adali420@frigg.humanoid.local>
2014-10-09 10:43:31 +0200

    This version can walk and talk

--- Assignment_2/Assignment_2_1.py
+++ Assignment_2/Assignment_2_1.py
@@ -11,8 +11,16 @@ from api.pubapi import sit, stand , rest, say , shutdown ,\
 # Define the functions for world model
 def detectTouch(wm):
     return readWM(wm, "tactile", "middle")
+# Adjusts the walking time of the robot
+def entryTime(wm):
+    return readWM(wm, "time", "entry")
+def currentTime(wm):
+    return readWM(wm, "time", "current")
 def walkTime(wm):
-    return readWM(wm, "time", "entry + 5")
+    if currentTime(wm) - entryTime(wm) >= 10:
+        return True
+    else:
+        return False
 
 ## CREATE STATES ##
 
@@ -26,18 +34,19 @@ shutdownState = createState("shutdownState",
 
 # Create states for talking
 
-sayLetsWalkState = createState("sayLetsWalkState", lambda : say("Lets walk!"))
+sayLetsWalkState = createState("sayLetsWalkState", lambda : say("Let us promenade!"))
+sayRotateState = createState("sayRotateState", lambda : say("Let us spinn!"))
 sayGoodbyeState = createState("sayGoodbyeState", lambda : say("Goodbye World!"))
  
 # Create states for waiting for touch
 
 waitSittingState = createState("waitSittingState", lambda : None)
-#waitStandingState = createState("waitStandingState", lambda : None)
+waitStandingState = createState("waitStandingState", lambda : None)
 
 # Add transitions according to the state diagram
 addTransition(waitSittingState , detectTouch , standState)
 addTransition(standState , lambda wm: True , sayLetsWalkState)
-addTransition(standState , lambda wm: True , walkState)
+addTransition(sayLetsWalkState, lambda wm: True , walkState)
 addTransition(walkState , walkTime , stopWalkState)
 addTransition(stopWalkState, lambda wm: True , sayGoodbyeState)
 addTransition(sayGoodbyeState , lambda wm: True , sitState)
@@ -47,7 +56,8 @@ addTransition(restState , lambda wm: True , shutdownState)
 # Create the FSM and add the states created above
 myFSM = createFSM("fsm")
 addStates(myFSM , waitSittingState , waitStandingState ,  standState , 
-          sayHelloState , sayGoodbyeState , sitState , restState , shutdownState)
+          sayLetsWalkState , sayGoodbyeState , sitState , restState ,
+          shutdownState , walkState , stopWalkState)
 
 # Set the initial state to waitSittingState
 setInitialState(myFSM , waitSittingState)
