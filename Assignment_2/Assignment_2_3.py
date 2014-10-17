d4bd9dcb2f3f93ad3ccd6644417b016ad75690e4
Yousif <youto814@loke.humanoid.local>
2014-10-17 11:58:59 +0200

    Completed 2.3, follows the ball without spasming

--- Assignment_2/Assignment_2_3.py
+++ Assignment_2/Assignment_2_3.py
@@ -16,24 +16,26 @@ def detectTouch(wm):
 
 def seeBall(wm):
     camera_data_balls = readWM(wm, "balls")
-    print (largestBall(wm)["pa"])
-    if largestBall(wm)["pa"] >= 70:
-        print( largestBall(wm)["pa"], "see ball" )
-        return True
+    if not largestBall(wm)["pa"] == None:
+        print (largestBall(wm)["pa"])
+        if largestBall(wm)["pa"] >= 70:
+            print( largestBall(wm)["pa"], "see ball" )
+            return True
     else:
         return False
 
 def noSeeBall(wm):
     camera_data_balls = readWM(wm, "balls")
-    print (largestBall(wm)["pa"])
-    if largestBall(wm)["pa"] <= 70:
-        print( largestBall(wm)["pa"] , "no ball")
-        return True
+    if not largestBall(wm)["pa"] == None:
+        print (largestBall(wm)["pa"])
+        if largestBall(wm)["pa"] <= 70:
+            print( largestBall(wm)["pa"] , "no ball")
+            return True
     else:
         return False
 
 def largestBall(wm):
-    camera_data_balls  = readWM ( wm , "balls" )
+    camera_data_balls  = readWM(wm,"balls")
     if not camera_data_balls:
         return None
     else:
@@ -41,14 +43,36 @@ def largestBall(wm):
         cur_frame = camera_data_balls[0]
 
         # The object with the largest area is most likely the true ball
-        largest_ball = None
-        for b in cur_frame:
-            if ( not largest_ball ) or (largest_ball["pa"] < b["pa"]):
-                largest_ball = b
-        return largest_ball
+        largest_ball1 = None
+        for b1 in cur_frame:
+            if ( not largest_ball1 ) or (largest_ball1["pa"] < b1["pa"]):
+                largest_ball1 = b1
+        return largest_ball1
+
+def averageLook(wm):
+    camera_data_balls = readWM(wm, "balls")
+    if not camera_data_balls:
+        return None
+    else:
+        largest_ball2 = None
+        second_frame = camera_data_balls[1]
+        for b in second_frame:
+            if (not largest_ball2) or (largest_ball2["pa"] < b["pa"]):
+                largest_ball2 = b
+        largest_ball3 = None
+        third_frame = camera_data_balls[2]
+        for c in third_frame:
+            if (not largest_ball3) or (largest_ball3["pa"] < c["pa"]):
+                largest_ball3 = c
+
+        pitch_average = (largestBall(wm)["pitch"] + largest_ball2["pitch"] +
+                        largest_ball3["pitch"]) / 3
+        yaw_average = (largestBall(wm)["yaw"] + largest_ball2["yaw"] + 
+                       largest_ball3["yaw"]) / 3
+        return [yaw_average, pitch_average]
 
 def lookAtBall(wm):
-    return turnHead(largestBall(wm)["yaw"], largestBall(wm)["pitch"])
+    return turnHead(averageLook(wm)[0], averageLook(wm)[1])
 
 def entryTime(wm):
     return readWM(wm, "time", "entry")
@@ -80,15 +104,18 @@ setTopCameraState = createState("setTopCameraState", lambda : setCamera("top"))
 setBottomCameraState = createState("setBottomCameraState", lambda : setCamera("bottom"))
 sayBallState = createState("sayBallState", lambda : say("ball!"))
 sayNoBallState = createState("sayNoBallState", lambda : say("no fucking ball found!"))
+lookAtBallState = createState("lookAtBallState", lambda wm : lookAtBall(wm) )
+
 
 # Add transitions
 
 addTransition(waitSittingState , detectTouch , standState)
-addTransition(standState, lambda wm: True, waitStandingState )
+addTransition(standState, lambda wm: True, hangHeadState )
+addTransition(hangHeadState, lambda wm: True, waitStandingState)
 addTransition(waitStandingState, seeBall  , sayBallState )
 addTransition(waitStandingState, time , sayNoBallState)
-addTransition(sayBallState, lambda wm: True, waitStanding2State )
-addTransition(waitStanding2State, noSeeBall , sayNoBallState )
+addTransition(sayBallState, lambda wm: True, lookAtBallState )
+addTransition(lookAtBallState, noSeeBall , sayNoBallState )
 addTransition(sayNoBallState, lambda wm: True, sitState)
 addTransition(sitState , lambda wm: True , restState)
 addTransition(restState , lambda wm: True , shutdownState)
@@ -98,9 +125,9 @@ myFSM = createFSM("fsm")
 addStates(myFSM , waitSittingState , standState , 
            sitState , restState , shutdownState ,
           shakeHeadState , shakeHead2State , 
-          hangHeadState, waitStandingState, waitStanding2State,
-          setTopCameraState, setBottomCameraState, sayBallState,
-          sayNoBallState)
+          hangHeadState, waitStandingState, setTopCameraState, 
+          setBottomCameraState, sayBallState,
+          sayNoBallState, lookAtBallState )
 
 setPrintTransition(myFSM, True)
 
